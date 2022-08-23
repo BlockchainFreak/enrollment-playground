596ee2e6d5f8b05df63f13b32c255142acacfb71
Umer Naeem <25100029@lums.edu.pk>
2022-08-24 04:12:07 +0500

    preview attempt 2

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Enrollment Helper</title>
-        <meta property='og:image' content='../data/preview.jpg' />
+        <meta property='og:image' content='https://i.ibb.co/BtfJZL8/preview.jpg'/>
         <meta property='og:description' content="Find clash-free schedules"/>
       </Head>
       <ThemeProvider theme={theme}>
